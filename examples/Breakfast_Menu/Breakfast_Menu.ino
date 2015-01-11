d48cb7f2193bdc197daed75a2ac1de2568d4518f
code@lookout.no <code@lookout.no>
2015-01-11 21:03:07 +0100

    Adding license info and aligning setup with other related library samples

--- examples/Breakfast_Menu/Breakfast_Menu.ino
+++ examples/Breakfast_Menu/Breakfast_Menu.ino
@@ -1,8 +1,30 @@
+/************************************************************************/
+/* MicroXPath lib, an XML navigator with a tiny memory footprint, v1.0. */
+/*                                                                      */
+/* This library is free software: you can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation, either version 3 of the License, or    */
+/* (at your option) any later version.                                  */
+/*                                                                      */
+/* This library is distributed in the hope that it will be useful, but  */
+/* WITHOUT ANY WARRANTY; without even the implied warranty of           */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU     */
+/* General Public License for more details.                             */
+/*                                                                      */
+/* You should have received a copy of the GNU General Public License    */
+/* along with this library. If not, see <http://www.gnu.org/licenses/>. */
+/*                                                                      */
+/* Written by Thomas Mittet (code@lookout.no) December 2014.            */
+/************************************************************************/
+
 #include <SPI.h>
 #include <Ethernet.h>
 #include <MicroXPath.h>
 
-#define ETHERNET_MAC (byte[]) { 0x36, 0xE6, 0xF2, 0x2E, 0x0E, 0xD3 }
+#define ETHERNET_MAC (byte[]) {0x54, 0x48, 0x4F, 0x4D, 0x41, 0x53}
+#define ETHERNET_STATIC_IP  (byte[]) {192, 168, 0, 123}
+#define ETHERNET_ERROR_DHCP "Error: DHCP"
+#define ETHERNET_ERROR_CONNECT "Error: Connect"
 
 EthernetClient client;
 MicroXPath xPath = MicroXPath();
@@ -16,22 +38,23 @@ void setup()
   {
     Serial.println("Connecting...");
     Serial.flush();
-    if (client.connect("www.w3schools.com", 80))
-    {
-      Serial.println("Connected: www.w3schools.com/xml/simple.xml");
-      client.println("GET /xml/simple.xml HTTP/1.1");
-      client.println("Host: www.w3schools.com");
-      client.println("Connection: close");
-      client.println();
-    }
-    else
-    {
-      Serial.println("Error: Connect");
-    }
   }
   else
   {
-    Serial.println("Error: DHCP");
+    Serial.println(ETHERNET_ERROR_DHCP);
+    Ethernet.begin(ETHERNET_MAC, ETHERNET_STATIC_IP);
+  }
+  if (client.connect("www.w3schools.com", 80))
+  {
+    Serial.println("Connected: www.w3schools.com/xml/simple.xml");
+    client.println("GET /xml/simple.xml HTTP/1.1");
+    client.println("Host: www.w3schools.com");
+    client.println("Connection: close");
+    client.println();
+  }
+  else
+  {
+    Serial.println(ETHERNET_ERROR_CONNECT);
   }
 }
 
